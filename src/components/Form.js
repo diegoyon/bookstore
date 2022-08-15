a2839e442111385cb76284f997002a9f0b74057d
Diego Yon <diegoyon@outlook.com>
2022-08-15 15:28:45 -0600

    Add functionality to add new book

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { useState, React } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { createBook } from '../redux/books/books';
 
-const Form = () => (
-  <>
-    <h1>Add new book</h1>
-    <input type="text" name="title" />
-    <input type="text" name="author" />
-    <button type="submit">Submit</button>
-  </>
-);
+const Form = () => {
+  const [titleInput, setTitleInput] = useState('');
+  const [authorInput, setAuthorInput] = useState('');
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h1>Add new book</h1>
+      <input type="text" name="title" value={titleInput} onInput={(e) => setTitleInput(e.target.value)} />
+      <input type="text" name="author" value={authorInput} onInput={(e) => setAuthorInput(e.target.value)} />
+      <button
+        onClick={() => {
+          dispatch(createBook({ title: titleInput, author: authorInput, id: uuidv4() }));
+          setTitleInput('');
+          setAuthorInput('');
+        }}
+        type="submit"
+      >
+        Submit
+      </button>
+    </>
+  );
+};
 
 export default Form;
