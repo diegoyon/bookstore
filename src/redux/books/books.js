ea4c11e9a7ca31f6d91f00337a8f057bcd1e40c3
Diego Yon <diegoyon@outlook.com>
2022-08-15 14:24:50 -0600

    Change the default state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,12 +1,26 @@
+// Initial State
+const defaultState = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+  },
+  {
+    id: 2,
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+  },
+];
+
 // Actions
 const CREATE = 'CREATE';
 const REMOVE = 'REMOVE';
 
 // Reducer
-export default function booksReducer(state = [], action = {}) {
+export default function booksReducer(state = defaultState, action = {}) {
   switch (action.type) {
     case CREATE:
-      return state.concat([action.book]);
+      return state.concat(action.book);
     case REMOVE:
       return state.filter((book) => JSON.stringify(book) !== JSON.stringify(action.book));
     default:
