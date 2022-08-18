5d68108e21e8cda8555804b1f5430f57a2645e79
Diego Yon <diegoyon@outlook.com>
2022-08-18 11:09:15 -0600

    Add styles to the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,7 @@ import { useState, React } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addNewBook } from '../redux/books/booksSlice';
+import './Form.css';
 
 const Form = () => {
   const [titleInput, setTitleInput] = useState('');
@@ -10,21 +11,25 @@ const Form = () => {
 
   return (
     <>
-      <h1>Add new book</h1>
-      <input type="text" name="title" value={titleInput} onInput={(e) => setTitleInput(e.target.value)} />
-      <input type="text" name="author" value={authorInput} onInput={(e) => setAuthorInput(e.target.value)} />
-      <button
-        onClick={() => {
-          dispatch(addNewBook({
-            item_id: uuidv4(), title: titleInput, author: authorInput, category: 'Fiction',
-          }));
-          setTitleInput('');
-          setAuthorInput('');
-        }}
-        type="submit"
-      >
-        Submit
-      </button>
+      <div className="form">
+        <h1>Add new book</h1>
+        <div className="form-container">
+          <input type="text" name="title" placeholder="Book title" value={titleInput} onInput={(e) => setTitleInput(e.target.value)} />
+          <input type="text" name="author" placeholder="Author" value={authorInput} onInput={(e) => setAuthorInput(e.target.value)} />
+          <button
+            onClick={() => {
+              dispatch(addNewBook({
+                item_id: uuidv4(), title: titleInput, author: authorInput, category: 'Fiction',
+              }));
+              setTitleInput('');
+              setAuthorInput('');
+            }}
+            type="submit"
+          >
+            ADD BOOK
+          </button>
+        </div>
+      </div>
     </>
   );
 };
